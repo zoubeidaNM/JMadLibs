a718030814a99fe0b869ba128eb129b5797845ba
zoubeidaNM <zoumenz@yahoo.com>
2017-10-04 09:26:55 -0400

    Initial commit

---
+++ src/JMadlibs.java
@@ -0,0 +1,41 @@
+
+    import java.util.Scanner;
+
+
+    public class JMadLibs {
+        public static void main(String args[]) {
+
+            Scanner keyboard = new Scanner(System.in);
+
+            //Variable with default values
+
+            String name1 = "Amy";
+            String adjective1 = "great";
+            String location = "Montgomery County";
+            int time1 = 9;
+            int time2 = 5;
+            int duration = 8;
+            String adjective2 = "exited";
+            int numHwk = 10;
+            int numWeeks = 2;
+            String adjective3 = "funny";
+            String adjective4 = "energetic";
+            String verb = "code";
+
+
+
+            System.out.println("Enter a name");
+            adjective1 = keyboard.nextLine();
+
+
+            System.out.println("Enter a number: ");
+            customerID = keyboard.nextInt();
+            keyboard.nextLine();
+
+
+            System.out.println("\n\nbalbla :" + adjective1 + "balabla" + adjective2 );
+            System.out.println("\n\nbalbla :" + adjective1 + "balabla" + adjective2 );
+
+        }
+    }
+
